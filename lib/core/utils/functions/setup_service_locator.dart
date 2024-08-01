46874b4c81416372fb98d260228463e0edec1961
itiswd <itiswdev97@gmail.com>
2024-08-01 23:38:56 +0300

    new commit

---
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:bookify/core/utils/api_service.dart';
+import 'package:bookify/features/home/data/data_source/home_local_data_source.dart';
+import 'package:bookify/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:bookify/features/home/data/repos/home_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImp>(
+    HomeRepoImp(
+      homeLocalDataSource: HomeLocalDataSourceImp(),
+      homeRemoteDataSource: HomeRemoteDataSourceImp(
+        getIt.get<ApiService>(),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:bookify/core/utils/app_routers.dart';
+import 'package:bookify/core/utils/functions/setup_service_locator.dart';
+import 'package:bookify/core/utils/simple_bloc_observer.dart';
+import 'package:bookify/features/home/data/repos/home_repo_imp.dart';
 import 'package:bookify/features/home/domain/entities/book_entity.dart';
+import 'package:bookify/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookify/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookify/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookify/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'consatants.dart';
@@ -9,8 +17,10 @@ import 'consatants.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
   await Hive.openBox<BookEntity>(kFeaturedBooks);
   await Hive.openBox<BookEntity>(kNewestBooks);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Bookify());
 }
 
@@ -25,15 +35,34 @@ class Bookify extends StatelessWidget {
         statusBarColor: Colors.transparent,
       ),
     );
-    return MaterialApp.router(
-      routerConfig: AppRouters.router,
-      title: 'Bookify',
-      theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(
+              getIt.get<HomeRepoImp>(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FetchNewestBooksUseCase(
+              getIt.get<HomeRepoImp>(),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouters.router,
+        title: 'Bookify',
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
