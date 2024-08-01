e842f86dc6a56fb0f0b44f0fae203fb2f9e6ed5e
itiswd <itiswdev97@gmail.com>
2024-08-01 22:39:20 +0300

    new commit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:bookify/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookify/features/home/domain/entities/book_entity.dart';
 import 'package:equatable/equatable.dart';
@@ -5,5 +6,14 @@ import 'package:equatable/equatable.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await featuredBooksUseCase.call();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
 }
