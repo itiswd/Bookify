3bbe6db3e3b39b29e3abac800e63d54b9260fb52
itiswd <itiswdev97@gmail.com>
2024-08-01 22:56:15 +0300

    new commit

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bookify/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bookify/features/home/domain/entities/book_entity.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errorMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,24 @@
+part of 'newest_books_cubit.dart';
+
+sealed class NewestBooksState extends Equatable {
+  const NewestBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksFailure extends NewestBooksState {
+  final String errorMessage;
+
+  const NewestBooksFailure(this.errorMessage);
+}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  const NewestBooksSuccess(this.books);
+}
